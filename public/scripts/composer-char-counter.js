5135d18ebb7d2666fca6358b69b8b6d1465c9d62
TinyMito <k.ip@me.com>
2023-05-16 22:46:26 -0400

    Updated .js comment

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,11 @@
 $(document).ready(function() {
+  // Monitor input from #tweet-text textarea
   $('#tweet-text').on('input', function() {
+    // textarea input
     let msg = $(this).val();
     let count = 140 - msg.length;
+
+    // Add class max if the character count reach less than 0 otherwise remove if over 0
     $('.counter').text(count);
     if (count < 0) {
       $('.counter').addClass('max');
