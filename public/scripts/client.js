5dce2e68e4a09186c2ff849200fd4b2ab7cee5a7
TinyMito <k.ip@me.com>
2023-05-16 23:24:30 -0400

    Change HTML structure

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,25 +4,16 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const createTweetElement = (data) => {
-  return data;
-}
+$(document).ready(function() {
+  const createTweetElement = () => {
+  }
+/* 
+  const $tweet = $(`
+  <article class="tweet-header">Hello world</article>
+  <article class="tweet-body">Hello world</article>
+  <article class="tweet-footer">Hello world</article>
+  `);
 
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-  "created_at": 1461116232227
-}
+  $('#tweets-container').append($tweet);  */
 
-const $tweet = createTweetElement(tweetData);
-
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+});
