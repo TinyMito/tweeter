cd39ee72dcfedd1e3bd01260057d05cdfd2caade
TinyMito <k.ip@me.com>
2023-05-17 00:23:10 -0400

    Add dynamic tweet functions

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,71 @@
  */
 
 $(document).ready(function() {
-  const createTweetElement = (data) => {
-    const created = timestamp(data.created_at);
-    const tweetHTML = $(`
-    <article class="tweet">
-    <div class="header">
-      <div class="user">
-        <img src="${data.user.avatars}"></img>
-        <span>${data.user.name}</span>
-      </div>
-      <span class="tag">${data.user.handle}</span>
-    </div>
-    <div class="body">
-      <span>${data.content.text}</span>
-    </div>
-    <div class="footer">
-      <span>${created} days ago</span>
-      <span><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></span>
-    </div>
-  </article>
-  `)
-  return tweetHTML;
+
+  // TEST DATA ONLY
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+
+  const renderTweets = (data) => {
+    // loops through tweets
+    for (let tweet of data) {
+      // calls createTweetElement for each tweet
+      const $tweet = createTweetElement(tweet);
+      // takes return value and appends it to the tweets container
+      $('#tweets-container').append($tweet); 
+    }
   };
 
+  const createTweetElement = (data) => {
+    // Call timestamp to covert to days
+    const created = timestamp(data.created_at);
+
+    // HTML ouput for tweet
+    const tweetHTML = $(`
+    <article class="tweet">
+      <div class="header">
+        <div class="user">
+          <img src="${data.user.avatars}"></img>
+          <span>${data.user.name}</span>
+        </div>
+        <span class="tag">${data.user.handle}</span>
+      </div>
+      <div class="body">
+        <span>${data.content.text}</span>
+      </div>
+      <div class="footer">
+        <span>${created} days ago</span>
+        <span><i class="fa-solid fa-flag"></i><i class="fa-solid fa-retweet"></i><i class="fa-solid fa-heart"></i></span>
+      </div>
+    </article>
+    `);
+
+    return tweetHTML;
+  };
+
+  // Timestamp to Days Converstion
   const timestamp = (time) => {
     const timestamp = time;
     const today = new Date();
@@ -38,22 +80,8 @@ $(document).ready(function() {
   
     // Convert to days
     return Math.floor(timeDiff / (1000 * 60 * 60 * 24));
-  }
+  };
 
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-    "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-    "created_at": 1461116232227
- }
-
-  const $tweet = createTweetElement(tweetData);
-
-  $('#tweets-container').append($tweet); 
+  renderTweets(data);
 
 });
